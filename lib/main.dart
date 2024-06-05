fa29320440ecf4d32eee4fccb9b8dce8d5a2fcbd
Arun Hegde <arunhegde@outlook.com>
2024-06-05 11:18:14 +0530

    added new navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 void main() {
   runApp(MyApp());
@@ -41,45 +42,51 @@ class _MyHomePageState extends State<MyHomePage> {
     ),
   ];
 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(30.0),
-          child: BottomNavigationBar(
-            backgroundColor: Colors.white,
-            elevation: 5.0,
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(Icons.timer_rounded),
-                label: 'Timer',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.checklist_rounded),
-                label: 'Tasks',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.stacked_bar_chart_rounded),
-                label: 'Stats',
-              ),
-            ],
-            currentIndex: _selectedIndex,
-            selectedItemColor: Colors.blue,
-            onTap: _onItemTapped,
-          ),
+        body: Center(
+          child: _widgetOptions.elementAt(_selectedIndex),
         ),
-      ),
-    );
+        bottomNavigationBar: Container(
+          decoration: BoxDecoration(color: Colors.white, boxShadow: [
+            BoxShadow(blurRadius: 20, color: Colors.black.withOpacity(0.2))
+          ]),
+          child: SafeArea(
+              child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+            child: GNav(
+              rippleColor: Colors.grey[300]!,
+              hoverColor: Colors.grey[200]!,
+              gap: 7,
+              activeColor: Colors.black,
+              iconSize: 24,
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+              duration: const Duration(milliseconds: 350),
+              tabBackgroundColor: Colors.grey[100]!,
+              color: Colors.black,
+              tabs: const [
+                GButton(
+                  icon: Icons.timer_rounded,
+                  text: 'Timer',
+                ),
+                GButton(
+                  icon: Icons.checklist_rounded,
+                  text: 'Tasks',
+                ),
+                GButton(
+                  icon: Icons.bar_chart_rounded,
+                  text: 'Tasks',
+                )
+              ],
+              selectedIndex: _selectedIndex,
+              onTabChange: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+            ),
+          )),
+        ));
   }
 }
